533ee559dcf696a00b35137d84bc1ab1c7273393
BrenoLeiriaoNeto <bleiriaoneto@icloud.com>
2025-09-03 15:04:29 -0300

    fix-project-scaffolding. fix applied

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
+    host: true,
+    port: 3000,
     proxy: {
-      '/api': 'http://localhost:8080',
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
     },
   },
   build: {
     outDir: 'dist',
     sourcemap: true,
   },
-})
+});
